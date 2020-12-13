eabffd988b3be3b7b55eacd525de4c5a788edd47
Hugo Carvalho <huguinho66@live.com>
2020-12-13 19:31:47 +0000

    First Commit

---
+++ Wolf_and_Sheeps_LP2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wolf_and_Sheeps_LP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
